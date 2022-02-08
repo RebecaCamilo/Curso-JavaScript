5c2e0005e2a12d03e39aa1c36cc5b73a63d9c85a
RebecaCamilo <rebecacamilo3@gmail.com>
2022-02-08 18:35:05 -0300

    variavel com let

---
+++ aula6 - variavel com let/index.js
@@ -0,0 +1,9 @@
+// let nome = 'Rebeca';
+let nome;
+nome = "Rebeca";
+
+console.log(nome);
+
+// console.log(nome, "nasceu em 1984.");
+// console.log(nome, "gosta de cupuaçu.");
+// console.log(nome, "estuda engenharia.");
