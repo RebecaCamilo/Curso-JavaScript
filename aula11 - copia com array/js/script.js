623598017997c4a6900fc4eae324f14c49125ab8
RebecaCamilo <rebecacamilo3@gmail.com>
2022-02-09 10:03:15 -0300

    copiar com array

---
+++ aula11 - copia com array/js/script.js
@@ -0,0 +1,6 @@
+let n1 = Number(prompt('digite um numero'))
+let n2 = Number(prompt('digite outro numero'))
+// let s = n1+n2
+
+// confirm('A soma do que vc digitou foi2 ' + s + '?')
+confirm(`A soma do que vc digitou foi2 ${n1+n2}?`)
