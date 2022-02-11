977705c07c091bba55a3f9b2f9e840f2c47ad8c0
Rebeca Maria Fialho Camilo <71824475+RebecaCamilo@users.noreply.github.com>
2022-02-11 16:26:47 -0300

    Delete aula6 - variavel com let directory

--- aula6 - variavel com let/index.js
+++
@@ -1,9 +0,0 @@
-// let nome = 'Rebeca';
-let nome;
-nome = "Rebeca";
-
-console.log(nome);
-
-// console.log(nome, "nasceu em 1984.");
-// console.log(nome, "gosta de cupuaçu.");
-// console.log(nome, "estuda engenharia.");
