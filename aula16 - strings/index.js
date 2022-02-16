85cbfc9fa2c4948fd654a7ab5a015343989ba601
RebecaCamilo <rebecacamilo3@gmail.com>
2022-02-16 15:14:30 -0300

    strings manipulation

---
+++ aula16 - strings/index.js
@@ -0,0 +1,70 @@
+let alunos = ['Maria', 'Jose', 'Joao'];
+// alunos = 1;
+
+alunos[2] = 'Artur';
+alunos[3] = 'Ana';
+
+
+console.log(typeof alunos);
+
+
+// console.log(typeof alunos);
+// console.log(alunos instanceof Array);
+
+
+
+// console.log(alunos);
+
+// console.log(alunos.slice(1, 3));
+// console.log(alunos.slice(0, -2));
+// console.log(alunos);
+
+
+
+// delete alunos[1];
+
+// console.log(alunos);
+// console.log(alunos[1]);
+
+
+
+// alunos.push('Miina');
+// alunos.push('Otto');
+
+// console.log(alunos[0]);
+
+// alunos.unshift('Zelda')
+
+// console.log(alunos[0]);
+
+// console.log(alunos);
+
+// const removidoFim = alunos.pop();
+
+// console.log(removidoFim);
+// console.log(alunos);
+
+
+// const removidoComeco = alunos.shift();
+
+// console.log(removidoComeco);
+// console.log(alunos);
+
+
+// alunos[9] = 'Aninha';
+
+// console.log(alunos.length)
+
+// console.log(alunos);
+// console.log(alunos[alunos.length-1]);
+
+// alunos[alunos.length] = 'Alex';
+// alunos[alunos.length] = 'Fabia';
+// alunos[alunos.length] = 'Luana';
+
+// console.log(alunos);
+// console.log(alunos[alunos.length-1]);
+
+// console.log(alunos[0]);
+// console.log(alunos[1]);
+// console.log(alunos[2]);
