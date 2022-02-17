a452c53a2975dcc192ab85f7e329d725a0a81159
RebecaCamilo <rebecacamilo3@gmail.com>
2022-02-17 09:39:33 -0300

    if and else

---
+++ aula17 - if e else/index.js
@@ -0,0 +1,11 @@
+let hora = 27;
+
+if(hora >= 0 && hora <= 11) {
+    console.log("Bom dia");
+} else if(hora >= 12 && hora <= 18) {
+    console.log("Boa tarde");
+} else if(hora >= 19 && hora <= 23) {
+    console.log("Boa noite");
+} else {
+    console.log("Hora inválida");
+} 
