33249e688ce30533852d5e7e750a0b27c4a05210
RebecaCamilo <rebecacamilo3@gmail.com>
2022-03-23 11:10:55 -0300

    learning about 'escopo lexico'

---
+++ aula29 - escopo lexico/index.js
@@ -0,0 +1,12 @@
+const nome = 'Ana';
+
+function falaNome() {
+    // const nome = 'Maria';
+    console.log(nome);
+}
+falaNome();
+
+function usaFalaNome() {
+    const nome = 'Maria';
+    falaNome()
+};
